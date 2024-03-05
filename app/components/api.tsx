51fc8e5f4889b2cc8d337e33eb8183e96299797b
IXIN <16cosu05mo30@gmail.com>
2024-03-05 20:09:11 +0900

    timeoutを15秒に

--- app/components/api.tsx
+++ app/components/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'https://spritter-backend.onrender.com/',
-  timeout: 10000,
+  timeout: 15000,
 });
 
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
