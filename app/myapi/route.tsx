29fb3e86b7f2ebe37d9e1e108e9957a9a26c1761
IXIN <16cosu05mo30@gmail.com>
2024-09-12 17:50:46 +0900

    myapi消したお

--- app/myapi/route.tsx
+++
@@ -1,9 +0,0 @@
-import { kv } from '@vercel/kv'; // SDKをインポート
-import { NextResponse } from 'next/server';
-
-export const GET = () => {
-  // // KVにデータをインサート。キー cart:123, 値 abc
-  // // KVからデータを取得
-  // const cart = await kv.get('cart:123');
-  return <></>;
-}
